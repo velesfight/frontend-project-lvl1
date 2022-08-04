b40099a471ec1e80a647920f0a3732d82595f2fe
Ekaterina <ekaterina@MacBook-Air-Ekaterina.local>
2022-08-04 11:10:01 +0300

    lvl6.4

--- games/calc.js
+++ games/calc.js
@@ -1,10 +1,11 @@
+import readlineSync from 'readline-sync';
 import generalGame from '../src/index.js';
 
-export default generalGame = () => {
+export default generalGame = (taskGame, gameRound) => {
 const taskGame = 'What is the result of the expression?';
 const gameQuestion = readlineSync.question(`Question: ${number}`);
 const youAnswer = readlineSync.question('Your answer: ');
-
+  const gameRound = () => {
   const Randnumber = () => Math.floor(Math.random() * 10);
   const number1 = Randnumber();
   const number2 = Randnumber();
@@ -12,7 +13,6 @@ const youAnswer = readlineSync.question('Your answer: ');
   const signs = Math.floor(Math.random() * sign.length);
   const signsRand = sign[signs];
   const number = `${number1} ${signsRand} ${number2}`;
-  const gameRound = () => {
   let answer = 0;
   switch (signsRand) {
     case '-':
@@ -27,6 +27,7 @@ const youAnswer = readlineSync.question('Your answer: ');
     default:
       answer = null;
   }
-  const round = (Number(youAnswer));
-  return(number, round)
-};
+    const round = (Number(youAnswer));
+  return generalGame(number, round);
+}
+
--- games/even.js
+++ games/even.js
@@ -1,14 +1,15 @@
+import readlineSync from 'readline-sync';
 import generalGame from '../src/index.js';
 
-export default generalGame = () => {
+export default generalGame = (taskGame, gameRound) => {
 const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const gameQuestion = readlineSync.question(`Question: ${number}`);
 const youAnswer = readlineSync.question('Your answer: ');
 
-const Randnumber = () => Math.floor(Math.random() * 10);
 const gameRound = () => {
+  const Randnumber = () => Math.floor(Math.random() * 10);
   const number = Randnumber();
   const numberEven = (number % 2 === 0);
   const round = numberEven ? 'yes' : 'no';
-  return (number, round);
+  return generalGame(number, round);
 };

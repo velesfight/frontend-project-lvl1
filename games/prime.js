10c3994e4ac99565a011c8129e29f580990f20bd
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-08-22 17:21:03 +0300

    prime2

--- games/prime.js
+++ games/prime.js
@@ -6,7 +6,7 @@ const primeNum = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i <= num; i += 1) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       return false;
     }

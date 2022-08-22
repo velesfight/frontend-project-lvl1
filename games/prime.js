3ef182b66d205fa55a85c6e548d7645962df014d
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-08-22 17:13:43 +0300

    prime1

--- games/prime.js
+++ games/prime.js
@@ -3,11 +3,11 @@ import getRandomInt from '../src/fails.js';
 
 const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 const primeNum = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i <= num; i += 1) {
     if (num % i === 0) {
-      return false;
-    }
-    if (num < 2) {
       return false;
     }
   }
